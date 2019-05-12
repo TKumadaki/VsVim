3cc2406bc948e7b497b46642729e7b14228fa3da
Kumadaki <kuma-501@space.ocn.ne.jp>
2019-05-12 23:46:27 +0900

    Add  EditorUtil.cs

---
+++ Src/VimApp/ScriptTest/EditorUtil.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VimApp.ScriptTest
+{
+    public class EditorUtil
+    {
+
+    }
+    public class EditorCoreUtil
+    {
+        public static bool IsEndPoint(SnapshotPoint point)
+        {
+            return point.Position == point.Snapshot.Length;
+        }
+    }
+}
